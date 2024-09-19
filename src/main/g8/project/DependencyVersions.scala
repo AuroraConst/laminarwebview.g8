695cb3436912f4b1b3286768aa9dba1531de6ee9
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-19 12:53:58 -0400

    Update DependencyVersions.scala

--- src/main/g8/project/DependencyVersions.scala
+++ src/main/g8/project/DependencyVersions.scala
@@ -3,7 +3,7 @@ object DependencyVersions {
   val scala              = "3.5.0"
   val scalajsdom        = "2.4.0"
   val laminar            = "16.0.0"
-  val laminext           = "0.15.0-M7"
+  val laminext           = "0.17.0"
   val `upickle`          = "3.1.0"
   val betterfiles        = "3.9.2"
   val scalatest          = "3.2.15"
